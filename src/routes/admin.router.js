7835d8c1e58b0e27354b03463328231a108a530d
katas8n1 <kirka6061512@icloud.com>
2024-11-30 08:00:22 +0100

    deleted shift

--- src/routes/admin.router.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.adminRouter = void 0;
-const express_1 = require("express");
-exports.adminRouter = (0, express_1.Router)();
-exports.adminRouter.post("/add-product");
-exports.adminRouter.post("/delete-product/:id");
